eccf7e46deda6ad8a36b196511f04ffe2965b64d
Evan Mulawski <1044791+EvanMulawski@users.noreply.github.com>
2023-06-04 13:05:03 -0400

    remove testing app

--- src/TestingApp/IRazerPwmFanController.cs
+++
@@ -1,8 +0,0 @@
-﻿internal interface IRazerPwmFanController
-{
-    bool Connect();
-    void Disconnect();
-    int GetChannelSpeed(int channel);
-    void SetChannelMode(int channel, byte mode);
-    void SetChannelPower(int channel, int power, byte command);
-}
